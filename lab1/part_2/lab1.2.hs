bdf52131e28049f1a94e885dda07b409a59e4587
Volodymyr Barybin <vohanchib@gmail.com>
2022-10-07 11:05:28 +0300

    added addtitonal line of comments

--- lab1/part_2/lab1.2.hs
+++ lab1/part_2/lab1.2.hs
@@ -1,6 +1,8 @@
 -- The Eq typeclass defines equality (==) and inequality (/=)
 -- The Fractional typeclass defines is equal to double or float
 -- Num is a numeric typeclass
+-- The line above means sum and interest equal to fractional, and periods any child of Num and Eq
+-- the func return result the same type as value t2
 deposit :: (Eq t1, Num t1, Fractional t2) => t2 -> t2 -> t1 -> t2
 deposit sum _ 0 = sum
 deposit sum interest periods = deposit (sum + (sum * interest/100)) interest (periods - 1)
